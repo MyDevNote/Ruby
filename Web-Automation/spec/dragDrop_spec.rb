88baefc33acea8f0248bef9147a20b14f1e0703a
Diego Fernandes <diegofernandes62@hotmail.com>
2020-09-21 11:21:50 -0300

    QA Update 21-09

--- Web-Automation/spec/dragDrop_spec.rb
+++ Web-Automation/spec/dragDrop_spec.rb
@@ -1,11 +1,20 @@
-describe 'Drag and drop', :drop do
+describe 'Drag and drop', :drag do
     
     before(:each)do
         visit 'https://training-wheels-protocol.herokuapp.com/drag_and_drop'
     end
 
     it 'Homem Aranha pertence ao time Stark' do
+
+        team_stark = find('.team-stark .column')   
+        team_cap = find('.team-cap .column')
         
+        spider = find('img[alt$=Aranha]')
+        spider.drag_to team_stark
+        
+        expect(team_stark).to have_css 'img[alt$=Aranha]'
+        expect(team_cap).not_to have_css 'img[alt$=Aranha]'
+
 
     end
 
